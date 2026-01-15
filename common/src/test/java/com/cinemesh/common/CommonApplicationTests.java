69f8341ca90bff89c5449dbb5bbbb720c71f6df8
truongbb <truongbb96@outlook.com>
2026-01-15 18:04:19 +0700

    feat: init repo

---
+++ common/src/test/java/com/cinemesh/common/CommonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cinemesh.common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ services/auth-service/src/main/java/com/cinemesh/authservice/AuthServiceApplication.java
@@ -0,0 +1,13 @@
+package com.cinemesh.authservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthServiceApplication.class, args);
+    }
+
+}
