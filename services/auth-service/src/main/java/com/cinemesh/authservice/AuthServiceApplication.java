16d1bbe5430424ec0f8324322ea15c800bc374b0
truongbb <truongbb96@outlook.com>
2026-03-25 17:47:17 +0700

    chore(deployment): reorganize auth-service and config-server for kustomization and update common pom

--- services/auth-service/src/main/java/com/cinemesh/authservice/AuthServiceApplication.java
+++ services/auth-service/src/main/java/com/cinemesh/authservice/AuthServiceApplication.java
@@ -2,10 +2,10 @@ package com.cinemesh.authservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication(scanBasePackages = "com.cinemesh")
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
 public class AuthServiceApplication {
 
     public static void main(String[] args) {
