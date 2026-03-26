4ea10f42a5cf126973ac680b3b3d513a35e6d977
truongbb <truongbb96@outlook.com>
2026-03-26 11:43:10 +0700

    chore(movie-service): configure for production and kustomization deployment

--- services/movie-service/src/main/java/com/cinemesh/movieservice/MovieServiceApplication.java
+++ services/movie-service/src/main/java/com/cinemesh/movieservice/MovieServiceApplication.java
@@ -2,8 +2,10 @@ package com.cinemesh.movieservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication(scanBasePackages = "com.cinemesh")
+//@EnableDiscoveryClient
 public class MovieServiceApplication {
 
     public static void main(String[] args) {
