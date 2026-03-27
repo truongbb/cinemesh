95ee60ffd6ccf0787c6afe170654769abd3c80d5
truongbb <truongbb96@outlook.com>
2026-03-27 17:52:07 +0700

    chore: enable feign clients and update discovery configuration for services

--- services/booking-service/src/main/java/com/cinemesh/bookingservice/BookingServiceApplication.java
+++ services/booking-service/src/main/java/com/cinemesh/bookingservice/BookingServiceApplication.java
@@ -2,11 +2,13 @@ package com.cinemesh.bookingservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableScheduling;
+//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @EnableScheduling
-//@EnableFeignClients
+@EnableFeignClients
+//@EnableDiscoveryClient
 @SpringBootApplication(scanBasePackages = "com.cinemesh")
 public class BookingServiceApplication {
 
