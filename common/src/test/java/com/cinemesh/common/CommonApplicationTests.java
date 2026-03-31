73aaae09d5de91c3aba32092a20443f0951997dd
truongbb <truongbb96@outlook.com>
2026-03-31 09:29:49 +0700

    test(common): disable spring cloud config in tests to prevent startup failure

--- common/src/test/java/com/cinemesh/common/CommonApplicationTests.java
+++ common/src/test/java/com/cinemesh/common/CommonApplicationTests.java
@@ -3,7 +3,10 @@ package com.cinemesh.common;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+        "spring.cloud.config.enabled=false",
+        "spring.cloud.config.import-check.enabled=false"
+})
 class CommonApplicationTests {
 
     @Test
